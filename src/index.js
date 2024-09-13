38917ce55bfe891ca1c292a7f744f306bf25049a
Leojb2oo8 <118618337+Leojb2oo8@users.noreply.github.com>
2024-09-13 14:13:46 +0000

    logging prototipe start

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
-import { feedPopulation, changePopulation } from "./Utils/creature-util.js";
-import { born } from "./Utils/general.js";
-import { deleteDeadTrees, regenTreeFood, reproduceTrees } from "./Utils/tree-util.js";
-import {
-  creatureStartPopulation,
-  treeStartPopulation,
-  maxDays,
-  creaturePopulation,
-  treePopulation,
-} from "./variables.js";
+// import { feedPopulation, changePopulation } from "./Utils/creature-util.js";
+// import { born } from "./Utils/general.js";
+// import { deleteDeadTrees, regenTreeFood, reproduceTrees } from "./Utils/tree-util.js";
+// import {
+//   creatureStartPopulation,
+//   treeStartPopulation,
+//   maxDays,
+//   creaturePopulation,
+//   treePopulation,
+// } from "./variables.js";
 
-born(creatureStartPopulation, "Creature");
-born(treeStartPopulation, "Tree");
+// born(creatureStartPopulation, "Creature");
+// born(treeStartPopulation, "Tree");
 
-let itreations = 0;
-console.log(creaturePopulation.sort((a, b) => a.n - b.n), treePopulation);
-while (itreations <= maxDays && creaturePopulation.length != 0) {
-  treePopulation.forEach(regenTreeFood)
-  feedPopulation()
-  changePopulation();
-  treePopulation.forEach(reproduceTrees)
-  deleteDeadTrees()
-  itreations++;
-  console.log(creaturePopulation.sort((a, b) => b.n - a.n), treePopulation, creaturePopulation.length,"\n///////////////////////////////////////////NEW DAY///////////////////////////////////////////");
-}
+// let itreations = 0;
+// console.log(creaturePopulation.sort((a, b) => a.n - b.n), treePopulation);
+// while (itreations <= maxDays && creaturePopulation.length != 0) {
+//   treePopulation.forEach(regenTreeFood)
+//   feedPopulation()
+//   changePopulation();
+//   treePopulation.forEach(reproduceTrees)
+//   deleteDeadTrees()
+//   itreations++;
+//   console.log(creaturePopulation.sort((a, b) => b.n - a.n), treePopulation, creaturePopulation.length,"\n///////////////////////////////////////////NEW DAY///////////////////////////////////////////");
+// }
 
-console.log("Creature POP: "+creaturePopulation.length+"\nTree POP: "+treePopulation.length);
+// console.log("Creature POP: "+creaturePopulation.length+"\nTree POP: "+treePopulation.length);
+
+
+
+import fs from 'fs';
+
+let data = "Log123"
+let data2 = "\ngayyy"
+
+fs.writeFile('Log.txt', data, (err) => {
+    if (err) throw err;
+})
+fs.appendFile("Log.txt", data2, (err)=>{
+  if (err) throw err;
+})
+
